ac3f57d89056186b30bcbcb64210f7121d0109ed
Reemabdulrahman <113185117+Reemabdulrahman@users.noreply.github.com>
2022-10-07 16:32:01 +0100

    Add files via upload

---
+++ includes/navbar.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-info">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Crud application</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <li class="nav-item">
+          <a class="nav-link" href="viewmedicine.php">view all medicine</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="addmedicine.php">Add medicine</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="updatemedicine.php">update medicine</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="deletemedicine.php">delete medicine</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="selectmedicine.php">select medicine</a>
+        </li>
+     
+
+      
+    </div>
+  </div>
+</nav>
